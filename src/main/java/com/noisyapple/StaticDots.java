3b9293fb2a91172d7617039da63962b30ac55b65
NoisyApple <leonel.aguirre77@gmail.com>
2020-09-13 20:28:11 -0500

    Dots drawing done.

--- src/main/java/com/noisyapple/StaticDots.java
+++ src/main/java/com/noisyapple/StaticDots.java
@@ -1,6 +1,9 @@
 package com.noisyapple;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,6 +14,10 @@ public class StaticDots extends JFrame {
 
   // Keeps track of width and height from constructor.
   private int width, height;
+
+  // Control constants.
+  private final int DOT_AMOUNT = 100;
+  private final int DOT_MAX_RADIUS = 20;
 
   // Class constructor.
   public StaticDots(int width, int height) {
@@ -34,8 +41,31 @@ public class StaticDots extends JFrame {
   // Inner class, models a custom canvas where to draw.
   class CustomCanvas extends JPanel {
 
-    // paint method inherited from Canvas, YOUR CAN DRAW HERE.
+    // paintComponent method inherited from Canvas, YOUR CAN DRAW HERE.
     public void paintComponent(Graphics g) {
+
+      // Graphics context g is then casted to Graphics2D.
+      Graphics2D g2 = (Graphics2D) g;
+
+      // Context configuration, antialiasing enabled for smoother graphics.
+      g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+      // Canvas background.
+      g2.setColor(Color.decode("#000000"));
+      g2.fillRect(0, 0, width, height);
+
+      // Dots color.
+      g2.setColor(Color.decode("#ffffff"));
+
+      for (int i = 0; i < DOT_AMOUNT; i++) {
+        int x = (int) ((Math.random() * width)); // Generates a x-coordinate value.
+        int y = (int) ((Math.random() * height)); // Generates a y-coordinate value.
+        int radius = (int) ((Math.random() * DOT_MAX_RADIUS) + 1); // Generates a radius value.
+
+        // A dot is drawn.
+        g2.fillOval(x, y, radius, radius);
+      }
+
     }
 
     // getPreferredSize gets overriden to make JFrame fit JPanel size;
